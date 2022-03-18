effe3c2e98e5abe6ac34d9d720329dc36d087f50
Jamie Smith <jamie.smith@cloudreach.com>
2022-03-18 11:20:06 +0000

    Add support for zips

--- dist/index.js
+++ dist/index.js
@@ -5415,15 +5415,30 @@ const path = __nccwpck_require__(17);
 const core = __nccwpck_require__(127);
 const tc = __nccwpck_require__(348);
 
-async function downloadTool(tool, url) {
+async function downloadToolTar(tool, url) {
   core.debug(`Downloading ${tool} CLI from ${url}`);
   const pathToCLITar = await tc.downloadTool(url);
 
-  core.debug(`Extracting ${tool} zip file`);
+  core.debug(`Extracting ${tool} tar file`);
   const pathToCLI = await tc.extractTar(pathToCLITar);
   core.debug(`${tool} path is ${pathToCLI}.`);
 
   if (!pathToCLITar || !pathToCLI) {
+    throw new Error(`Unable to download ${tool} from ${url}`);
+  }
+
+  return pathToCLI;
+}
+
+async function downloadToolZip(tool, url) {
+  core.debug(`Downloading ${tool} CLI from ${url}`);
+  const pathToCLIZip = await tc.downloadTool(url);
+
+  core.debug(`Extracting ${tool} zip file`);
+  const pathToCLI = await tc.extractZip(pathToCLIZip);
+  core.debug(`${tool} path is ${pathToCLI}.`);
+
+  if (!pathToCLIZip || !pathToCLI) {
     throw new Error(`Unable to download ${tool} from ${url}`);
   }
 
@@ -5437,15 +5452,15 @@ async function run() {
     core.debug(`Getting download URL for terraform-docs version ${tfDocsInputVersion}`);
     const tfdocsURL = `https://github.com/terraform-docs/terraform-docs/releases/download/${tfDocsInputVersion}/terraform-docs-${tfDocsInputVersion}-linux-amd64.tar.gz`;
 
-    const pathToTFDocs = await downloadTool("terraform-docs", tfdocsURL);
+    const pathToTFDocs = await downloadToolTar("terraform-docs", tfdocsURL);
 
     core.addPath(pathToTFDocs);
 
     const tfLintInputVersion = core.getInput('tflint_version');
     core.debug(`Getting download URL for tflint version ${tfLintInputVersion}`);
-    const tflintURL = `https://github.com/terraform-linters/tflint/releases/download/${tfLintInputVersion}/tflint_linux_amd64.tar.gz`;
+    const tflintURL = `https://github.com/terraform-linters/tflint/releases/download/${tfLintInputVersion}/tflint_linux_amd64.zip`;
 
-    const pathToTFLint = await downloadTool("tflint", tflintURL);
+    const pathToTFLint = await downloadToolZip("tflint", tflintURL);
 
     core.addPath(pathToTFLint);
   } catch (ex) {
--- src/setup-dependencies.js
+++ src/setup-dependencies.js
@@ -4,15 +4,30 @@ const path = require('path');
 const core = require('@actions/core');
 const tc = require('@actions/tool-cache');
 
-async function downloadTool(tool, url) {
+async function downloadToolTar(tool, url) {
   core.debug(`Downloading ${tool} CLI from ${url}`);
   const pathToCLITar = await tc.downloadTool(url);
 
-  core.debug(`Extracting ${tool} zip file`);
+  core.debug(`Extracting ${tool} tar file`);
   const pathToCLI = await tc.extractTar(pathToCLITar);
   core.debug(`${tool} path is ${pathToCLI}.`);
 
   if (!pathToCLITar || !pathToCLI) {
+    throw new Error(`Unable to download ${tool} from ${url}`);
+  }
+
+  return pathToCLI;
+}
+
+async function downloadToolZip(tool, url) {
+  core.debug(`Downloading ${tool} CLI from ${url}`);
+  const pathToCLIZip = await tc.downloadTool(url);
+
+  core.debug(`Extracting ${tool} zip file`);
+  const pathToCLI = await tc.extractZip(pathToCLIZip);
+  core.debug(`${tool} path is ${pathToCLI}.`);
+
+  if (!pathToCLIZip || !pathToCLI) {
     throw new Error(`Unable to download ${tool} from ${url}`);
   }
 
@@ -26,15 +41,15 @@ async function run() {
     core.debug(`Getting download URL for terraform-docs version ${tfDocsInputVersion}`);
     const tfdocsURL = `https://github.com/terraform-docs/terraform-docs/releases/download/${tfDocsInputVersion}/terraform-docs-${tfDocsInputVersion}-linux-amd64.tar.gz`;
 
-    const pathToTFDocs = await downloadTool("terraform-docs", tfdocsURL);
+    const pathToTFDocs = await downloadToolTar("terraform-docs", tfdocsURL);
 
     core.addPath(pathToTFDocs);
 
     const tfLintInputVersion = core.getInput('tflint_version');
     core.debug(`Getting download URL for tflint version ${tfLintInputVersion}`);
-    const tflintURL = `https://github.com/terraform-linters/tflint/releases/download/${tfLintInputVersion}/tflint_linux_amd64.tar.gz`;
+    const tflintURL = `https://github.com/terraform-linters/tflint/releases/download/${tfLintInputVersion}/tflint_linux_amd64.zip`;
 
-    const pathToTFLint = await downloadTool("tflint", tflintURL);
+    const pathToTFLint = await downloadToolZip("tflint", tflintURL);
 
     core.addPath(pathToTFLint);
   } catch (ex) {
