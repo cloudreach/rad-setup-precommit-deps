8b0d410a6f59bded27a6a1ec545008ac34c2ca48
Jamie Smith <jamie.smith@cloudreach.com>
2022-03-18 10:18:28 +0000

    feat: Initial commit of action

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # rad-setup-precommit-deps
 Gtihub Action to set up dependencies used for precommit checks in pipelines
+
+## Inputs
+| Name | Description | Default |
+| ---- | ----------- |---------|
+| tflint_version | TFLint version to install | v0.34.1 | 
+| terraform_docs_version | terrafrom-docs version to install | latest |
+
+## Example Workflow
+```yaml
+name: Static Testing
+on: [pull_request]
+jobs:
+  pre-commit:
+    runs-on: ubuntu-latest
+    steps:
+    - uses: actions/checkout@v2
+    - name: Set up Python 3.8
+      uses: actions/setup-python@v1
+      with:
+        python-version: 3.8
+    - uses: cloudreach/rad-setup-precommit-deps@master
+      name: Setup precommit dependencies
+      with:
+        tflint_version: v0.34.1
+    - name: Pre-commit
+      run: |
+        pip install pre-commit
+        pre-commit run --all-files
+```
---
+++ src/setup-dependencies.js
@@ -0,0 +1,46 @@
+const os = require('os');
+const path = require('path');
+
+const core = require('@actions/core');
+const tc = require('@actions/tool-cache');
+
+async function downloadTool(tool, url) {
+  core.debug(`Downloading ${tool} CLI from ${url}`);
+  const pathToCLITar = await tc.downloadTool(url);
+
+  core.debug(`Extracting ${tool} zip file`);
+  const pathToCLI = await tc.extractTar(pathToCLITar);
+  core.debug(`${tool} path is ${pathToCLI}.`);
+
+  if (!pathToCLITar || !pathToCLI) {
+    throw new Error(`Unable to download ${tool} from ${url}`);
+  }
+
+  return pathToCLI;
+}
+
+async function run() {
+  try {
+    const tfDocsInputVersion = core.getInput('terraform_docs_version');
+
+    core.debug(`Getting download URL for terraform-docs version ${tfDocsInputVersion}`);
+    const tfdocsURL = `https://github.com/terraform-docs/terraform-docs/releases/download/${tfDocsInputVersion}/terraform-docs-${tfDocsInputVersion}-linux-amd64.tar.gz`;
+
+    const pathToTFDocs = await downloadTool("terraform-docs", tfdocsURL);
+
+    core.addPath(pathToTFDocs);
+
+    const tfLintInputVersion = core.getInput('tflint_version');
+    core.debug(`Getting download URL for tflint version ${tfLintInputVersion}`);
+    const tflintURL = `https://github.com/terraform-linters/tflint/releases/download/${tfLintInputVersion}/tflint-linux-amd64.tar.gz`;
+
+    const pathToTFLint = await downloadTool("tflint", tflintURL);
+
+    core.addPath(pathToTFLint);
+  } catch (ex) {
+    core.error(ex);
+    throw ex;
+  }
+}
+
+module.exports = run;
